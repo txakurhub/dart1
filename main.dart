b42555205337777d681a57d1553c55f28a754b10
unknown <me.leandrop@gmail.com>
2023-05-02 19:39:40 -0300

    int double

--- main.dart
+++ main.dart
@@ -1,8 +1,13 @@
 void main() {
   // Strings
-  //  --inferido
   final String nombre = "Tony";
   String apellido = "García";
 
   print('$nombre $apellido');
+
+  // Números
+  int empleados = 30;
+  double salario = 1856.25;
+
+  print('$empleados $salario');
 }
